7e78844dccd40390f709b43ed5a2758d45d9c05f
huaruiwu <huarui.wu@gmail.com>
2014-08-19 02:38:51 -0700

    add lockfile to server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var fs = require('fs');
+var lockFile = require('lockFile');
 var app = express();
 var STATUS_FILE_PATH = __dirname+'/data/status.json';
 
@@ -7,7 +8,9 @@ app.get('/', function(req, res) {
   fs.readFile(STATUS_FILE_PATH, function(error, data) {
     res.json(JSON.parse(data));
     console.log('sent');
+    lockFile.lockSync('stats.lock');
     fs.writeFileSync(STATUS_FILE_PATH, '[]');
+    lockFile.unlockSync('stats.lock');
   });
 });
 
