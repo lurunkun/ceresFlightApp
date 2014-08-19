9d778ae6a820715f2257d55610b8f9fb57d6be7c
huaruiwu <huarui.wu@gmail.com>
2014-08-19 01:17:25 -0700

    add filelock

--- server/testWriter.js
+++ server/testWriter.js
@@ -1,4 +1,7 @@
 var fs = require('fs');
+var lockFile = require('lockfile');
+
+fs.truncateSync(__dirname+'/data/status.json', 0);
 
 // create status.json array if empty
 var buff = fs.readFileSync(__dirname+'/data/status.json')
@@ -7,29 +10,29 @@ if (buff.length === 0) {
 }
 
 setInterval(function() {
-  fs.readFile(__dirname+'/data/status.json', function(err, status) {
-    var status = JSON.parse(status);
-    fs.readFile(__dirname+'/data/dummyStatus.json', function(readErr, dummyStatus) {
-      if (readErr) console.log(readErr);
-      status.push(JSON.parse(dummyStatus));
-      fs.writeFile(__dirname+'/data/status.json', JSON.stringify(status), function(writeErr) {
-        if (writeErr) console.log(writeErr);
-        console.log('write dummy status');
+      lockFile.lock('status.lock', function() {
+        writeStats('dummyStatus');
       });
-    });
-  });
-}, 500);
+}, 5);
 
 setInterval(function() {
+      lockFile.lock('status.lock', function() {
+        writeStats('dummyError');
+      });
+}, 20);
+
+
+function writeStats(type) {
   fs.readFile(__dirname+'/data/status.json', function(err, status) {
     var status = JSON.parse(status);
-    fs.readFile(__dirname+'/data/dummyError.json', function(readErr, dummyError) {
+    fs.readFile(__dirname+'/data/' + type + '.json', function(readErr, dummyData) {
       if (readErr) console.log(readErr);
-      status.push(JSON.parse(dummyError));
+      status.push(JSON.parse(dummyData));
       fs.writeFile(__dirname+'/data/status.json', JSON.stringify(status), function(writeErr) {
         if (writeErr) console.log(writeErr);
-        console.log('write dummy error');
+        console.log('write dummy ' + type);
       });
     });
   });
-}, 5000);
+}
+
