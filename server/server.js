48fc731584c78fa2a02ddb2fd40441da6e71c77a
huaruiwu <huarui.wu@gmail.com>
2014-08-25 23:17:45 -0700

    add route for restart

--- server/server.js
+++ server/server.js
@@ -25,7 +25,17 @@ app.get('/', function(req, res) {
 });
 
 app.get('/reboot', function(req, res) {
-
+  sleep.sleep(20);
+  try {
+    lockFile.lockSync('status.lock', opt);
+    fs.writeFileSync(STATUS_FILE_PATH, '[]');
+    lockFile.unlockSync('status.lock');
+  } catch(e) {
+    console.log(e);
+    res.json([]);
+  } finally {
+    return;
+  }
 });
 
 // testWriter();
