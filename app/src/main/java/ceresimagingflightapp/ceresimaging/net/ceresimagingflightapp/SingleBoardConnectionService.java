5149a33d6f812171eb41850e38497f3ed8b6951e
huaruiwu <huarui.wu@gmail.com>
2014-10-02 00:02:39 -0700

    remove timer function for data retrieve

--- app/src/main/java/ceresimagingflightapp/ceresimaging/net/ceresimagingflightapp/SingleBoardConnectionService.java
+++ app/src/main/java/ceresimagingflightapp/ceresimaging/net/ceresimagingflightapp/SingleBoardConnectionService.java
@@ -1,16 +1,13 @@
 package ceresimagingflightapp.ceresimaging.net.ceresimagingflightapp;
 
-import android.annotation.TargetApi;
 import android.app.Service;
 import android.content.Intent;
-import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.squareup.otto.Bus;
 
-import org.apache.http.conn.HttpHostConnectException;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -18,11 +15,8 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.Socket;
-import java.net.SocketTimeoutException;
 import java.util.ArrayList;
-import java.util.Timer;
 import java.util.TimerTask;
 
 public class SingleBoardConnectionService extends Service {
@@ -30,7 +24,6 @@ public class SingleBoardConnectionService extends Service {
     private static final String SBC_URL = "192.168.1.11";
     private static final int SBC_PORT = 9000;
     private static final String TAG = "Ceres SBC Connection Service";
-    private static Timer timer = new Timer();
     private static MainThreadBus mBus = new MainThreadBus(new Bus());
 
     static boolean inError = false;
@@ -50,7 +43,6 @@ public class SingleBoardConnectionService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Toast.makeText(this, "service started", Toast.LENGTH_SHORT).show();
-        mRetrieveDataTask = createRetrieveDataTask();
         retrieveDataFromSBC();
         return Service.START_STICKY;
     }
@@ -58,8 +50,6 @@ public class SingleBoardConnectionService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
-        SingleBoardConnectionService.timer.cancel();
-        SingleBoardConnectionService.timer.purge();
         Toast.makeText(this, "service destroyed", Toast.LENGTH_SHORT).show();
     }
 
@@ -79,81 +69,52 @@ public class SingleBoardConnectionService extends Service {
         inError = false;
     }
 
-    private static void pauseRetrieveDataTask() {
-        SingleBoardConnectionService.timer.cancel();
-        SingleBoardConnectionService.timer.purge();
-    }
-    private static void resumeRetrieveDataTask() {
-        SingleBoardConnectionService.timer = new Timer();
-        mRetrieveDataTask = createRetrieveDataTask();
-        SingleBoardConnectionService.timer.scheduleAtFixedRate(mRetrieveDataTask, 0, UPDATE_INTERVAL);
-    }
-
-    private static TimerTask createRetrieveDataTask() {
-        return new TimerTask() {
-            @TargetApi(Build.VERSION_CODES.KITKAT)
-            @Override
+    private void retrieveDataFromSBC() {
+        new Thread() {
             public void run() {
-//                InputStream content;
-                try {
-//                    HttpClient httpClient = new DefaultHttpClient();
-//                    HttpResponse response = httpClient.execute(new HttpGet("http://" + SBC_URL + SBC_PORT));
-//                    content = response.getEntity().getContent();
-                    Socket socket = new Socket(SBC_URL, SBC_PORT);
-                    BufferedReader read = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    StringBuilder contentString = new StringBuilder();
-                    String line;
-                    while ((line = read.readLine()) != null) {
-                        contentString.append(line);
-                    }
-                    // parse the string retrieved to JSON Array
+                while(true) {
                     try {
-                        JSONArray statusArray = new JSONArray(contentString.toString());
-                        for (int i=0; i < statusArray.length(); i++) {
-                            JSONObject statusObject = statusArray.getJSONObject(i);
-                            // send statusObject event through eventBus
-                            mBus.post(new SingleBoardDataEvent(statusObject));
-                            String status = statusObject.getString("type");
-                            // if received error
-                            if (status.equals(SingleBoardStatus.ERROR)) {
-                                SingleBoardConnectionService.inError = true;
-                                mErrors.add(new SingleBoardDataEvent(statusObject));
-                            // if received warning
-                            } else if (status.equals(SingleBoardStatus.WARNING)) {
-                                SingleBoardConnectionService.inWarning = true;
-                            }
+                        Socket socket = new Socket(SBC_URL, SBC_PORT);
+                        BufferedReader read = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        StringBuilder contentString = new StringBuilder();
+                        String line;
+                        while ((line = read.readLine()) != null) {
+                            contentString.append(line);
                         }
-                        mBus.post(new SingleBoardConnectionEvent(true));
-                    } catch (JSONException e) {
-                        Log.e(TAG, "JSON Status Parse Failed.", e);
+                        // parse the string retrieved to JSON Array
+                        try {
+                            JSONArray statusArray = new JSONArray(contentString.toString());
+                            for (int i=0; i < statusArray.length(); i++) {
+                                JSONObject statusObject = statusArray.getJSONObject(i);
+                                // send statusObject event through eventBus
+                                mBus.post(new SingleBoardDataEvent(statusObject));
+                                String status = statusObject.getString("type");
+                                // if received error
+                                if (status.equals(SingleBoardStatus.ERROR)) {
+                                    SingleBoardConnectionService.inError = true;
+                                    mErrors.add(new SingleBoardDataEvent(statusObject));
+                                    // if received warning
+                                } else if (status.equals(SingleBoardStatus.WARNING)) {
+                                    SingleBoardConnectionService.inWarning = true;
+                                }
+                            }
+                            mBus.post(new SingleBoardConnectionEvent(true));
+                        } catch (JSONException e) {
+                            Log.e(TAG, "JSON Status Parse Failed.", e);
+                        }
+                        break;
+                    } catch (IOException e) {
+                        Log.w(TAG, "LOG Warning connecting to SBC");
+                        e.printStackTrace();
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e1) {
+                            Log.e(TAG, "LOG Error socket connect sleep error");
+                            e1.printStackTrace();
+                        }
                     }
-                } catch (MalformedURLException e) {
-                    Log.e(TAG, "LOG MalformedURLException", e);
-                    mBus.post(new SingleBoardConnectionEvent(false));
-                    SingleBoardConnectionService.inError = false;
-                    SingleBoardConnectionService.inWarning = false;
-                } catch (SocketTimeoutException e) {
-                    Log.e(TAG, "LOG Error timed out", e);
-                    mBus.post(new SingleBoardConnectionEvent(false));
-                    SingleBoardConnectionService.inError = false;
-                    SingleBoardConnectionService.inWarning = false;
-                } catch (HttpHostConnectException e) {
-                    Log.e(TAG, "LOG connection refused");
-                    mBus.post(new SingleBoardConnectionEvent(false));
-                    SingleBoardConnectionService.inError = false;
-                    SingleBoardConnectionService.inWarning = false;
-                } catch (IOException e) {
-                    Log.w(TAG, "LOG Error connecting to service", e);
-                    mBus.post(new SingleBoardConnectionEvent(false));
-                    SingleBoardConnectionService.inError = false;
-                    SingleBoardConnectionService.inWarning = false;
-                } finally {
                 }
             }
-        };
-    }
-
-    private void retrieveDataFromSBC() {
-        SingleBoardConnectionService.timer.scheduleAtFixedRate(mRetrieveDataTask, 0, UPDATE_INTERVAL);
+        }.start();
     }
 }
