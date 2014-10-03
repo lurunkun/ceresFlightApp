deace10fbd51b6f0e66e540e408cab200d722566
huaruiwu <huarui.wu@gmail.com>
2014-10-02 21:40:13 -0700

    add location event

---
+++ app/src/main/java/ceresimagingflightapp/ceresimaging/net/ceresimagingflightapp/tabletGPSDataEvent.java
@@ -0,0 +1,16 @@
+package ceresimagingflightapp.ceresimaging.net.ceresimagingflightapp;
+
+import android.location.Location;
+
+/**
+ * Created by huaruiwu on 10/2/14.
+ */
+public class tabletGPSDataEvent {
+
+    public Location location;
+
+    public tabletGPSDataEvent(Location location) {
+        this.location = location;
+    }
+
+}
