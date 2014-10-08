8733cee76c952294808af6e622577310a4e08bae
huaruiwu <huarui.wu@gmail.com>
2014-10-07 23:22:33 -0700

    add receiveServer using boost

--- server/cpp/receiveServer.cpp
+++ server/cpp/receiveServer.cpp
@@ -10,10 +10,21 @@ int main() {
         tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 3000));
 
         while (true) {
+            // get connection
             tcp::socket socket(io_service);
             acceptor.accept(socket);
             boost::system::error_code ignored_error;
 
+            // read stream
+            boost::asio::streambuf b;
+            boost::asio::read_until(socket, b, "\n");
+
+            // print string
+            std::istream is(&b);
+            std::string line;
+            std::getline(is, line);
+
+            std::cout << line << std::endl;
         }
 
     } catch (std::exception& e) {
