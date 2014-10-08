f58ec31acd5f7a13dbb5693f8c0c9827aa766f37
huaruiwu <huarui.wu@gmail.com>
2014-10-08 00:16:52 -0700

    finish send receive and add makefile

--- app/src/main/java/ceresimagingflightapp/ceresimaging/net/ceresimagingflightapp/SingleBoardConnectionService.java
+++ app/src/main/java/ceresimagingflightapp/ceresimaging/net/ceresimagingflightapp/SingleBoardConnectionService.java
@@ -10,7 +10,7 @@ import com.squareup.otto.Bus;
 import java.util.ArrayList;
 
 public class SingleBoardConnectionService extends Service {
-    public static final String SBC_URL = "192.168.1.165";
+    public static final String SBC_URL = "192.168.1.13";
 //    public static final String SBC_URL = "192.168.1.231";
     public static final int RECEIVE_PORT = 9000;
     public static final int SEND_PORT = 3000;
---
+++ server/cpp/sendServer.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+int main() {
+
+    std::ifstream ifs;
+    std::string status;
+    std::string line;
+    ifs.open("./data/dummyStatus.json");
+    while (std::getline(ifs, line)) {
+        status += line;
+    }
+
+    try {
+        boost::asio::io_service io_service;
+        tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 9000));
+
+        while (true) {
+            // get connection
+            tcp::socket socket(io_service);
+            acceptor.accept(socket);
+
+            // read stream
+            while (true) {
+                boost::system::error_code ignored_error;
+                boost::asio::write(socket, boost::asio::buffer("[" + status + "]\n"), ignored_error);
+                std::cout << "[" << status << "]" << std::endl;
+                // pause 1 second
+                sleep(1);
+            }
+
+        }
+
+    } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
